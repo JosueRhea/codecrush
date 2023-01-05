5e9e0f52784a47ebceb386fd780d9a60be4bf86b
Josue A <73492768+JosueRhea@users.noreply.github.com>
2023-01-05 20:27:14 +0000

    Update: config change

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["turbo", "prettier", "next"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",

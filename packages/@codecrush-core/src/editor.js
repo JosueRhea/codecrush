54c880d79b41c0b0732b4d6f458918481a37c2a2
Josue A <73492768+JosueRhea@users.noreply.github.com>
2023-01-05 21:13:21 +0000

    Chore: display the text

--- packages/@codecrush-core/src/editor.js
+++ packages/@codecrush-core/src/editor.js
@@ -2,7 +2,7 @@ import { EditableInput } from "./editableInput";
 
 export class Editor {
   constructor() {
-    this.editorContent = "";
+    this.editorContent = null;
     this.editorEl = null;
     this.textarea = null;
     this.isFocus = false;
@@ -10,10 +10,24 @@ export class Editor {
   }
 
   #setupEditor() {
+    //Parent element
     const editor = document.createElement("div");
     editor.setAttribute("class", "text-editor");
     editor.setAttribute("tabindex", "0");
-    document.body.appendChild(editor);
+    
+    // Pre element
+    const pre = document.createElement("pre");
+    pre.setAttribute("class", "text-editor-content");
+    editor.appendChild(pre)
+
+    //Code
+    const code = document.createElement("code");
+    code.setAttribute("class", "text-editor-code");
+    pre.appendChild(code)
+
+    document.body.appendChild(editor)
+
+    this.editorContent = code
     this.editorEl = editor;
   }
 
@@ -27,12 +41,16 @@ export class Editor {
     });
 
     hiddenInput.onChange((e) => {
-      console.log(e);
+      this.#displayText(e)
     });
 
     hiddenInput.onBlur(() => {
       this.isFocus = false;
       this.editorEl.classList.remove('focused')
     });
+  }
+
+  #displayText(value){
+    this.editorContent.textContent = value
   }
 }

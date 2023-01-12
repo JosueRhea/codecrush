e7c87fe6402d0ca8ee1ef9763cdecbc7ecdc9076
JosueRhea <josueroyale8171@gmail.com>
2023-01-12 14:43:02 -0600

    Chore: selection to left added

--- packages/@codecrush-core/src/Selection.js
+++ packages/@codecrush-core/src/Selection.js
@@ -4,22 +4,54 @@ export class Selection extends Component {
   constructor() {
     super();
     this.range = null;
+    this.isShiftKey = false;
+  }
+
+  onKeyPressed(key, _, shiftKey) {
+    if (shiftKey) this.isShiftKey = true;
+    else {
+      this.isShiftKey = false;
+      this.deselectText();
+    }
   }
 
   onPositionChange(data) {
-    console.log(data);
+    if (!this.isShiftKey) return;
+    const isLeft = data.before.line.position > data.after.line.position;
+    if (isLeft) {
+      const lineIndex = data.before.line.index;
+      this.selectTextToLeft(
+        lineIndex,
+        data.after.line.position,
+        data.before.line.position
+      );
+    }
   }
 
-  selectTextToLeft() {
-    const currentLine = this.editor.lines[this.editor.currentLineIndex];
+  selectTextToLeft(lineIndex, start, end) {
+    const currentLine = this.editor.lines[lineIndex];
     if (this.editor.isSelecting) {
+      this.editor.editorSelection.map((selection) => {
+        if (selection.lineIndex === lineIndex) {
+          selection.start = start;
+          const width = currentLine.getOffsetSumRange(
+            selection.start,
+            selection.end
+          );
+          const left = currentLine.getOffsetSum(selection.start);
+          selection.width = width;
+          selection.left = left;
+          this.editor.editorSelection.push(selection);
+          this.renderSelection();
+        }
+      });
     } else {
+      this.editor.isSelecting = true;
       const selection = {
         lineIndex: this.editor.currentLineIndex,
-        start: this.editor.currentPositionOnLine - 1,
-        end: this.editor.currentPositionOnLine,
+        start: start,
+        end: end,
       };
-      console.log(selection);
       const width = currentLine.getOffsetSumRange(
         selection.start,
         selection.end
@@ -33,22 +65,21 @@ export class Selection extends Component {
   }
 
   deselectText() {
-    // console.log("Calling deselect");
     if (this.editor.isSelecting) {
       this.editor.isSelecting = false;
-      //   this.editor.lines.forEach((line) => {
-      //     if (line.selection) {
-      //       line.selection = null;
-      //       line.selectionEl.style.top = "";
-      //       line.selectionEl.style.left = "";
-      //       line.selectionEl.style.width = "";
-      //       line.selectionEl.style.height = "";
-      //     }
-      //   });
+      this.editor.editorSelection.forEach(({ lineIndex }) => {
+        const line = this.editor.lines[lineIndex];
+        line.selection = null;
+        line.selectionEl.style.top = "";
+        line.selectionEl.style.left = "";
+        line.selectionEl.style.width = "";
+        line.selectionEl.style.height = "";
+      });
+      this.editor.editorSelection.length = 0;
     }
   }
 
-  renderSelection(line) {
+  renderSelection() {
     this.editor.editorSelection.forEach(({ lineIndex, width, left }) => {
       const line = this.editor.lines[lineIndex];
       const linePos = line.getPosition();

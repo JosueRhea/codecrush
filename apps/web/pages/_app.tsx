b4ddd1896a76ee349d6dc8e5f53820cc8369ee8f
JosueRhea <josueroyale8171@gmail.com>
2023-01-19 11:32:51 -0600

    Chore: testing ssr

---
+++ apps/web/pages/_app.tsx
@@ -0,0 +1,5 @@
+import "@codecrush/core/dist/index.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,8 +1,12 @@
 import { Button } from "ui";
-import { initEditor } from '@codecrush/core'
+// import { initEditor } from "@codecrush-core";
+import { initEditor } from "@codecrush/core";
+import { useEffect } from "react";
 
 export default function Web() {
-  console.log(initEditor)
+  useEffect(() => {
+    console.log(initEditor());
+  }, []);
   return (
     <div>
       <h1>Web</h1>

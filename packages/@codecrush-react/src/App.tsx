c49a0542575e1dd6195a8cebef0306c318608c52
JosueRhea <josueroyale8171@gmail.com>
2023-01-21 15:05:13 -0600

    Fix: packages name conflict

---
+++ packages/@codecrush-react/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Editor } from "./components";
+
+function App() {
+  return (
+    <div className="App">
+      <Editor />
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/@codecrush-react/src/components/Editor.tsx
@@ -0,0 +1,8 @@
+import { initEditor } from "codecrush-core";
+
+const Editor = () => {
+  console.log(initEditor);
+  return <div>Editor</div>;
+};
+
+export default Editor;

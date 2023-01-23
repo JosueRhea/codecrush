fea9daf7a3ece978014c53eedb8287caaada7f96
JosueRhea <josueroyale8171@gmail.com>
2023-01-23 09:16:31 -0600

    Chore: testing

--- packages/@codecrush-react/src/App.tsx
+++ packages/@codecrush-react/src/App.tsx
@@ -4,8 +4,13 @@ import "codecrush-core/dist/index.css";
 
 function App() {
   return (
-    <div className="code-editor">
-      <Editor height={300} id={"js-editor"} theme="material-darker" />
+    <div className="grid">
+      <div className="code-editor">
+        <Editor height={300} id={"js-editor"} theme="material-darker" />
+      </div>
+      <div className="code-editor">
+        <Editor height={300} id={"another-editor"} theme="dracula-soft" />
+      </div>
     </div>
   );
 }

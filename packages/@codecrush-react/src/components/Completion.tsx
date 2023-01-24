2fa7428ec5a69240aa34ebcea802579ee71f18bb
JosueRhea <josueroyale8171@gmail.com>
2023-01-24 12:05:53 -0600

    Chore: testing components call

--- packages/@codecrush-react/src/components/Completion.tsx
+++ packages/@codecrush-react/src/components/Completion.tsx
@@ -1,12 +1,13 @@
-import { Component } from "codecrush-core";
+import { Component, NavigationComponent } from "codecrush-core";
 
 export class CohereAutoCompletion extends Component {
   constructor() {
-    super('cohere-autocompletion');
+    super("cohere-autocompletion");
   }
 
   onKeyPressed(data: string, withCtrlKey: boolean, shiftKey: boolean): void {
     // const autoCompletion = this.editor
-    console.log(this.editor);
+    // const autocompletion =
+    //   this.editor.getComponent<NavigationComponent>("navigation");
   }
 }

c01702ea989eceea5f7e37252d114a045d99e12c
JosueRhea <josueroyale8171@gmail.com>
2023-01-25 10:40:07 -0600

    Chore: add abort controller for the plugin

--- apps/web/editorPlugins/AiAutocompletion.ts
+++ apps/web/editorPlugins/AiAutocompletion.ts
@@ -7,47 +7,53 @@ import debounce from "../utils/debounce";
 
 export class CohereAutoCompletion extends Component {
   getResultsFromAi: any;
+  signal: AbortController | null;
   constructor() {
     super("cohere-autocompletion");
+    this.signal = null;
 
     this.init();
   }
 
   init() {
     this.getResultsFromAi = debounce(() => {
-      if (process.env.NEXT_PUBLIC_COHERE_TOKEN) {
-        const completion =
-          this.editor.getComponent<AutocompletionComponent>("autocompletion");
-        const activityBar =
-          this.editor.getComponent<ActivityBarComponent>("activity-bar");
-
-        activityBar.registerActivity(
-          "cohere-activity",
-          "Getting autocompletions..."
-        );
-        this.fetchAutoCompletions(completion.currentWord)
-          .then((res) => res.json())
-          .then((data) => {
-            const suggestions = data.generations[0].text
-              .replace("\n", "")
-              .replace("--", "")
-              .split(",")
-              .map((x: string) => x.trim())
-              .filter((x: string) => x !== "");
-            suggestions.forEach((item: string) => {
-              completion.results.push({ suggestion: item, owner: "co:here" });
-            });
-            completion.render();
-            activityBar.removeActivity("cohere-activity");
-          })
-          .catch(() => {
-            activityBar.removeActivity("cohere-activity");
-          });
+      if (!process.env.NEXT_PUBLIC_COHERE_TOKEN) return;
+      const completion =
+        this.editor.getComponent<AutocompletionComponent>("autocompletion");
+      const activityBar =
+        this.editor.getComponent<ActivityBarComponent>("activity-bar");
+      if (this.signal) {
+        this.signal.abort();
+        this.signal = null;
       }
+
+      activityBar.registerActivity(
+        "cohere-activity",
+        `co:here -> (${completion.currentWord})`
+      );
+      this.fetchAutoCompletions(completion.currentWord)
+        .then((res) => res.json())
+        .then((data) => {
+          const suggestions = data.generations[0].text
+            .replace("\n", "")
+            .replace("--", "")
+            .split(",")
+            .map((x: string) => x.trim())
+            .filter((x: string) => x !== "");
+          suggestions.forEach((item: string) => {
+            completion.results.push({ suggestion: item, owner: "co:here" });
+          });
+          completion.render();
+          activityBar.removeActivity("cohere-activity");
+        })
+        .catch(() => {
+          activityBar.removeActivity("cohere-activity");
+        });
     }, 500);
   }
 
   async fetchAutoCompletions(currentWord: string) {
+    this.signal = new AbortController();
     return fetch("https://api.cohere.ai/generate", {
       method: "POST",
       headers: {
@@ -56,6 +62,7 @@ export class CohereAutoCompletion extends Component {
         authorization: `Bearer ${process.env.NEXT_PUBLIC_COHERE_TOKEN}`,
         "content-type": "application/json",
       },
+      signal: this.signal.signal,
       body: JSON.stringify({
         max_tokens: 100,
         return_likelihoods: "NONE",
